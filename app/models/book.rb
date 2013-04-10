1522547c1d457e3e01d04d42c686e7ba8483c712
Eoghan Ó Carragáin <eoghan.ocarragain@gmail.com>
2013-04-10 19:08:02 +0100

    refs#4 override to_solr in book.rb to index Page text and added test which is passing

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,13 @@ class Book < ActiveFedora::Base
 
   delegate :title, to: 'descMetadata', unique: true
   delegate :author, to: 'descMetadata'
+  
+  def to_solr(solr_doc={})
+    super
+    solr_doc["text_tesim"] = []
+    self.pages.each do |page|
+      solr_doc["text_tesim"] << page.text[0]
+    end
+    return solr_doc
+  end
 end
