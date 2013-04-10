09bf1aec4f1029f679a3384fec0e5650c1ab062a
Eoghan Ó Carragáin <eoghan.ocarragain@gmail.com>
2013-04-10 22:46:03 +0100

    refs #4 move associated pages example into the existing to_solr describe block

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -14,36 +14,29 @@ describe Book do
     subject.title.should == "My Title"
   end
   
-  describe "to_solr" do
+  describe "#to_solr" do
     before do
-      subject.title = "My Title"
-      subject.author = "Author Name"
-      @solr_doc = subject.to_solr    
+      @book = Book.create(title: "My Title", author: "Author Name")  
+
+      # Create two Pages with different text, and link to the Book
+      Page.create(number: 1, text: "Lorem ipsum", book: @book)
+      Page.create(number: 2, text: "dolor", book: @book)
+      
+      # Need to reload the Book or test fails since @book.pages returns empty
+      @book = Book.find(@book.pid)
+      
+      @solr_doc = @book.to_solr    
     end
+    
     it "should set solr fields for our metadata" do
       @solr_doc["title_tesim"].should == ["My Title"]
       @solr_doc["author_tesim"].should == ["Author Name"]
       @solr_doc["title_si"].should == "My Title"
     end
-  end
-  
-  describe "should index page text" do
-    before do
-      # Create an instance of Book
-      subject = Book.create
-      
-      # Create two Pages with different text, and link to the Book
-      Page.create(number: 1, text: "Lorem ipsum", book: subject)
-      Page.create(number: 2, text: "dolor", book: subject)
-      
-      # Need to reload the Book or the test fails
-      subject = Book.find(subject.pid)
-      @solr_doc = subject.to_solr    
-    end
-    
-    it "should have all associated pages" do
+
+    it "should have text for all associated pages" do
       @solr_doc["text_tesim"].should include("Lorem ipsum")
       @solr_doc["text_tesim"].should include("dolor")
-    end  	  
+    end      
   end
 end
