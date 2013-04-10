4c8eba5e0fc2ae5b100c29c34227f16b8565af72
jpstroop <jpstroop@gmail.com>
2013-04-10 17:50:25 +0100

    Revert "Added test for parsign structure metadata"
    This reverts commit 920133bbc07a9f27042084d37e8fc36be7e39704.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,8 @@
 class Book < ActiveFedora::Base
   has_many :pages, :property => :is_part_of
   has_metadata 'descMetadata', type: Datastream::BookMetadata
-  has_metadata 'pageList', type: Datastream::StructureMetadata
 
 
   delegate :title, to: 'descMetadata', unique: true
   delegate :author, to: 'descMetadata'
-
-  delegate :page_list, to: 'pageList'
 end
