b262784fe108e7d20d36cd872066a07147b72785
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2013-04-11 15:24:20 +0100

    fixed Bug in StructureMetadata

--- app/models/datastream/structure_metadata.rb
+++ app/models/datastream/structure_metadata.rb
@@ -1,12 +1,11 @@
 class Datastream::StructureMetadata < ActiveFedora::OmDatastream
 
   set_terminology do |t|
-    t.root(path: "pages") {
-    t.page {
+    t.root(path: "pages") 
+    t.page_list(:path=>"page") {
       t.pid(path: {attribute: "pid"})
       t.order(path: {attribute: "order"})
     }
-  }
   end
 
   def self.xml_template
